1c42f9071d07463b8a6aa11e26a0a949a2b13edb
Gary Stafford <garystafford@rochester.rr.com>
2016-07-05 01:04:24 -0400

    Fixing configs

--- src/main/java/com/example/EurekaServerApplication.java
+++ src/main/java/com/example/EurekaServerApplication.java
@@ -2,12 +2,10 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
 @EnableEurekaServer
-@EnableDiscoveryClient
+@SpringBootApplication
 public class EurekaServerApplication {
 
     public static void main(String[] args) {
